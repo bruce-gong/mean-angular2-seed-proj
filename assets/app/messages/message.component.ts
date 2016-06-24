88ac7a0403ed2e5bd88197f58f9bf7a3c3ce17b8
Bruce Kung <bruce.kung34@gmail.com>
2016-06-23 23:01:43 -0400

    Add property&event binding

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "angular2/core";
+import {Component, Input, Output, EventEmitter} from "angular2/core";
 import {Message} from "./message"
 @Component({
     selector: 'my-message',
@@ -12,7 +12,7 @@ import {Message} from "./message"
                     {{message.username}}
                 </div>
                 <div class="config">
-                    <a href='#'>Edit</a>
+                    <a href='#' (click)="onClick()">Edit</a>
                     <a href='#'>Delete</a>
                 </div>
             </footer>
@@ -35,5 +35,10 @@ import {Message} from "./message"
 })
 
 export class MessageComponent {
-    message: Message = new Message('The content', null, 'Max');
+    @Input() message: Message;
+    @Output() editClicked = new EventEmitter<string>();
+
+    onClick() {
+        this.editClicked.emit('Changed');
+    }
 }
